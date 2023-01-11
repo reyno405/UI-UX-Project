6cc3d076f30f0de75d2839327c738bd08ea0244f
reyno405 <reyno405@gmail.com>
2023-01-11 12:33:01 -0500

    this is a update with nav bar css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
+import './App.css'
 
 const App = () => {
   return (
-    <div>
-      <h1>GPT-3</h1>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar></Navbar>
+        <Header></Header>
+      </div>
+      <Brand></Brand>
+      <WhatGPT3></WhatGPT3>
+      <Features></Features>
+      <Possibility></Possibility>
+      <CTA></CTA>
+      <Blog></Blog>
+      <Footer></Footer>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
